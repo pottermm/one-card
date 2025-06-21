aea02fdc63882b07aed726de9a18f926a78729ec
Mike Potter <pottermm@gmail.com>
2025-06-21 17:49:46 -0400

    Update client.js

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 TrelloPowerUp.initialize({
   'card-buttons': function(t, options){
     return [{
-      icon: 'https://pottermm.github.io/one-card/task.svg',
+      icon: 'https://pottermm.github.io/one-card/task.256x224.png',
       text: 'Create Daily Card',
     }];
   },
